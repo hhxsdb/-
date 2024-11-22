28fa6e25af2db576fa9e073037de7ce05ad846d9
ZNT <1772537473@qq.com>
2024-11-23 00:28:17 +0800

    完成了新闻添加模块的页面

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -2,34 +2,35 @@
     <div style="border: 1px solid #ccc">
         <Toolbar style="border-bottom: 1px solid #ccc" :editor="editorRef" :defaultConfig="toolbarConfig" :mode="mode" />
         <Editor style="height: 500px; overflow-y: hidden;" v-model="valueHtml" :defaultConfig="editorConfig" :mode="mode"
-            @onCreated="handleCreated" />
+            @onCreated="handleCreated" @onChange="handleEditorChange" />
     </div>
 </template>
 
 <script>
 import '@wangeditor/editor/dist/css/style.css' // 引入 css
 
-import { onBeforeUnmount, ref, shallowRef, onMounted } from 'vue'
+import { onBeforeUnmount, ref, shallowRef, onMounted, defineEmits } from 'vue'
 import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
 
 export default {
     components: { Editor, Toolbar },
-    setup() {
+    setup(props, { emit }) { // 注意这里的 { emit }
         // 编辑器实例，必须用 shallowRef
         const editorRef = shallowRef()
 
         // 内容 HTML
         const valueHtml = ref('<p>hello</p>')
 
-        // 模拟 ajax 异步获取内容
-        onMounted(() => {
-            setTimeout(() => {
-                valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
-            }, 1500)
-        })
-
+        // 编辑器配置
         const toolbarConfig = {}
         const editorConfig = { placeholder: '请输入内容...' }
+
+        // 监听编辑器内容变化的处理函数
+        const handleEditorChange = (editor) => {
+            const html = editor.getHtml()
+            // 使用 emit 来触发自定义事件
+            emit('event', html)
+        }
 
         // 组件销毁时，也及时销毁编辑器
         onBeforeUnmount(() => {
@@ -38,6 +39,7 @@ export default {
             editor.destroy()
         })
 
+        // 编辑器创建时，保存实例
         const handleCreated = (editor) => {
             editorRef.value = editor // 记录 editor 实例，重要！
         }
@@ -48,10 +50,11 @@ export default {
             mode: 'default', // 或 'simple'
             toolbarConfig,
             editorConfig,
-            handleCreated
+            handleCreated,
+            handleEditorChange // 返回处理函数
         };
     }
 }
-</script> 
+</script>
 
 <style lang="scss" scoped></style>
